d6a548de4c002272f41ca654647b77b0eea61f59
shashikgrp <shashi@kgrp.in>
2023-09-21 15:38:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Logo from './components/logo';
+
+function App() {
+  return (
+    <div className="App">
+      <Logo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/logo.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./logo.css";
+
+const Logo = () => {
+  return (
+    <div className = "Container">
+        <div className = "center-box">
+                 
+        <div className = "square">
+            <div className = "content-box">
+            <div className = "content-head">HTML <span className = "inner-content">&</span> CSS</div>
+            <div className = "content-text"> Desigen and Build Website</div>
+            </div>
+        </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Logo
